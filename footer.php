7e658bdfc3eb581fe672f5b25ac17447696de4ff
Bindiya Patoliya <bindiyapatoliyagithub@gmail.com>
2026-08-07 14:33:51 +0530

    Complete premium footer

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<?php get_template_part( 'template-parts/footer/site-footer' ); ?>
+
 <?php wp_footer(); ?>
 
 </body>
---
+++ template-parts/footer/site-footer.php
@@ -0,0 +1,59 @@
+<footer class="site-footer">
+
+    <div class="container">
+
+        <div class="footer-top">
+
+            <a href="#" class="footer-logo">
+
+                <img
+                    src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/logo.png' ); ?>"
+                    alt="CyberSplash">
+
+            </a>
+
+            <ul class="footer-menu">
+
+                <li><a href="#">Fashion</a></li>
+                <li><a href="#">Beauty</a></li>
+                <li><a href="#">Lifestyle</a></li>
+                <li><a href="#">Travel</a></li>
+
+            </ul>
+
+            <p class="footer-text">
+
+                Timeless fashion, inspiring modern living through editorial stories,
+                curated trends and elegant lifestyle inspiration.
+
+            </p>
+
+            <div class="footer-social">
+
+                <a href="#">Instagram</a>
+
+                <a href="#">Pinterest</a>
+
+                <a href="#">Facebook</a>
+
+                <a href="#">Twitter</a>
+
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+
+            <p>
+
+                © <?php echo esc_html( date( 'Y' ) ); ?>
+
+                CyberSplash. All Rights Reserved.
+
+            </p>
+
+        </div>
+
+    </div>
+
+</footer>
